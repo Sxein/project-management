8e03fa791b69a3456fe4023c1ccad99520994fc3
Zayar Shein <alexleotwd@gmail.com>
2025-11-28 23:19:58 +0800

    Style the Modal via Tailwind CSS

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
+import Button from "./Button.jsx";
 
 export default function Modal({ children, buttonCaption, ref }) {
   const dialog = useRef();
@@ -11,10 +12,13 @@ export default function Modal({ children, buttonCaption, ref }) {
     };
   });
   return createPortal(
-    <dialog ref={dialog}>
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+    >
       {children}
-      <form action="dialog">
-        <button>[{buttonCaption}]</button>
+      <form action="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
       </form>
     </dialog>,
     document.getElementById("modal-root")
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Input from "./Input.jsx";
 import Modal from "./Modal.jsx";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modalRef = useRef();
   const title = useRef();
   const description = useRef();
@@ -30,13 +30,22 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modalRef} buttonCaption="Close">
-        <h2>Invalid Input</h2>
-        <p>Oops.... looks like you forgot to fill in all fields.</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops.... looks like you forgot to fill in all fields.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure to provide a title, description, and due date for the
+          project.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16 ">
         <menu className="flex justify-end items-center gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-900">
+            <button
+              className="text-stone-800 hover:text-stone-900"
+              onClick={onCancel}
+            >
               Cancel
             </button>
           </li>
